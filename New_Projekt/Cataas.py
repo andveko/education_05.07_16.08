2ef4ef68a6a9d0a151c4894672e87dc716a3b8e0
andveko <anveko@list.ru>
2024-08-04 12:14:50 +0300

    Добавлено меню.

--- New_Projekt/Cataas.py
+++ New_Projekt/Cataas.py
@@ -41,9 +41,20 @@ window.geometry("600x550")
 label = Label()
 label.pack()
 
-# Добавляем кнопку для обновления изображения
-update_button = Button(text="Обновить", command=set_image)
-update_button.pack()
+# # Добавляем кнопку для обновления изображения
+# update_button = Button(text="Обновить", command=set_image)
+# update_button.pack()
+
+# Создаем меню
+menu_bar = Menu(window)
+window.config(menu=menu_bar)
+
+# Добавляем пункты меню
+file_menu = Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label="Файл", menu=file_menu)
+file_menu.add_command(label="Загрузить фото", command=set_image)
+file_menu.add_separator()
+file_menu.add_command(label="Выход", command=exit)
 
 # Кнопка "Выход"
 exit_button = Button(text='Выход', command=window.destroy)
