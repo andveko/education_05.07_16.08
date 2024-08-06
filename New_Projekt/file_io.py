ab3d62dd8908c883cc36ee4159b702e813c45a64
andveko <anveko@list.ru>
2024-08-06 18:54:40 +0300

    Добавлено копирование ссылки в буфер обмена.

--- New_Projekt/file_io.py
+++ New_Projekt/file_io.py
@@ -1,12 +1,17 @@
 from tkinter import *
 import requests
-from tkinter import filedialog as fd
-from tkinter import messagebox as mb
+# from tkinter import filedialog as fd
+# from tkinter import messagebox as mb
 from tkinter import ttk
+import requests
+import pyperclip
+from tkinter import filedialog, messagebox
+from tkinter import ttk
+import tkinter as tk
 
 def upload():
     try:
-        filepath = fd.askopenfilename()
+        filepath = filedialog.askopenfilename()
         if filepath:
             with open(filepath, 'rb') as f:
                 files = {'file': f}
@@ -16,22 +21,24 @@ def upload():
                 if download_link:
                     link_entry.delete(0, END)
                     link_entry.insert(0, download_link)
+                    pyperclip.copy(download_link)  # Копирование ссылки в буфер обмена
+                    messagebox.showinfo("Ссылка скопирована", "Ссылка успешно скопирована в буфер обмена")
                 else:
                     raise ValueError("Не удалось получить ссылку для скачивания")
     except ValueError as ve:
-        mb.showerror("Ошибка", f"Произошла ошибка: {ve}")
+        messagebox.showerror("Ошибка", f"Произошла ошибка: {ve}")
     except Exception as e:
-        mb.showerror("Ошибка", f"Произошла ошибка: {e}")
+        messagebox.showerror("Ошибка", f"Произошла ошибка: {e}")
 
 
 window = Tk()
 window.title("Сохранение файлов в облаке")
-window.geometry('400x200')
+window.geometry('300x200')
 
-upload_button = ttk.Button(text="Загрузить файл", width=29, command=upload)
+upload_button = ttk.Button(text="Загрузить файл", width=27, command=upload)
 upload_button.pack(padx=10, pady=10)
 
-link_entry = ttk.Entry(width=30)
+link_entry = ttk.Entry(width=28)
 link_entry.pack(padx=10, pady=10)
 
 # Кнопка "Выход"
