edf91f3c335a9566e458da98a5e781254d4405b7
andveko <anveko@list.ru>
2024-08-07 15:49:53 +0300

    Файл скопирован и в программу добавлены коментарии.

--- New_Projekt/file_io.py
+++ New_Projekt/file_io.py
@@ -22,6 +22,27 @@ def save_history(file_path, download_link):
         json.dump(history, file, indent=4)
 
 
+def show_history():
+    if not os.path.exists(history_file):
+        messagebox.showinfo("История", "История загрузок пуста")
+        return
+
+    history_window = Toplevel(window)
+    history_window.title("История Загрузок")
+
+    files_listbox = Listbox(history_window, width=50, height=20)
+    files_listbox.grid(row=0, column=0, padx=(10,0), pady=10)
+
+    links_listbox = Listbox(history_window, width=50, height=20)
+    links_listbox.grid(row=0, column=1, padx=(0,10), pady=10)
+
+    with open(history_file, "r") as file:
+        history = json.load(file)
+        for item in history:
+            files_listbox.insert(END, item['file_path'])
+            links_listbox.insert(END, item['download_link'])
+
+
 def upload():
     try:
         filepath = filedialog.askopenfilename()
@@ -55,11 +76,14 @@ upload_button.pack(padx=10, pady=10)
 link_entry = ttk.Entry(width=28)
 link_entry.pack(padx=10, pady=10)
 
+history_button = ttk.Button(window, text="Показать Историю", width=27, command=show_history)
+history_button.pack(padx=10, pady=10)
+
 # Кнопка "Выход"
 exit_button = ttk.Button(text='Выход', command=window.destroy)
 # Упаковываем кнопку "Выход".
 # Кнопку устанавливаем в правом нижнем углу, окна программы.
 # С отступами по горизонтали и вертикали.
-exit_button.pack(side=BOTTOM,padx=10, pady=(0, 10))
+exit_button.pack(side=BOTTOM, padx=10, pady=(0, 10))
 
 window.mainloop()
